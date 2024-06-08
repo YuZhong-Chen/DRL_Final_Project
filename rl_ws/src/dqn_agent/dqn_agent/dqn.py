2e317f73e63ee573882a1dfbf8cf60d11be9af4f
YuZhong-Chen <yuzhong1214@gmail.com>
2024-06-08 16:47:28 +0800

    Fix dueling dqn.

--- rl_ws/src/dqn_agent/dqn_agent/dqn.py
+++ rl_ws/src/dqn_agent/dqn_agent/dqn.py
@@ -55,9 +55,9 @@ class NETWORK(nn.Module):
 
         # Dueling DQN -> Q(s, a) = V(s) + A(s, a)
         advantage = nn.functional.leaky_relu(self.advantage1(feature_map))
-        advantage = nn.functional.leaky_relu(self.advantage2(advantage))
+        advantage = self.advantage2(advantage)
         value = nn.functional.leaky_relu(self.value1(feature_map))
-        value = nn.functional.leaky_relu(self.value2(value))
+        value = self.value2(value)
         q_value = value + advantage - advantage.mean(dim=1, keepdim=True)
 
         return q_value
